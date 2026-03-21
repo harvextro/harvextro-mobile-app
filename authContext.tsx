8965ea7265f1e7d4f6b53e1a181622dd8b89a35c
Manthisi Herath <manthisi.h@gmail.com>
2026-03-22 01:56:59 +0530

    Add AuthContextType interface for user and loading state

--- authContext.tsx
+++ authContext.tsx
@@ -1,3 +1,8 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { auth } from "@/config/firebase";
 import { User, onAuthStateChanged } from "firebase/auth";
+
+interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+}
