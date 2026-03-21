dd700a31c0c19a9732437a0e25d3a986ec037685
Manthisi Herath <manthisi.h@gmail.com>
2026-03-21 22:02:38 +0530

    Add comments

--- authService.js
+++ authService.js
@@ -7,19 +7,23 @@ import {
 } from "firebase/auth";
 import { auth } from "../config/firebase";
 
+// Email/Password register
 export const registerUser = (email, password) => {
   return createUserWithEmailAndPassword(auth, email, password);
 };
 
+// Email/Password login
 export const loginUser = (email, password) => {
   return signInWithEmailAndPassword(auth, email, password);
 };
 
+// Google login (works for login & register)
 const provider = new GoogleAuthProvider();
 export const loginWithGoogle = () => {
   return signInWithPopup(auth, provider);
 };
 
+// Logout
 export const logoutUser = () => {
   return signOut(auth);
 };
