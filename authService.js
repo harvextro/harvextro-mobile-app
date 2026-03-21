b194d178fa1c063bf3a33edd0b651a36c8073713
Manthisi Herath <manthisi.h@gmail.com>
2026-03-21 21:55:08 +0530

    Add registerUser function to authService

--- authService.js
+++ authService.js
@@ -6,3 +6,7 @@ import {
     signOut,
 } from "firebase/auth";
 import { auth } from "../config/firebase";
+
+export const registerUser = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
